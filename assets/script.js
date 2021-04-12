f1487348c83e99a8b6507a99017ef7a3949df8ac
Melvin Finn <melvinrf@Outlook.com>
2021-04-12 17:25:58 -0500

    got a start

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+var currentDay = $("#currentDay");
+
+var schedule = {
+    9: "",
+    10: "",
+    11: "",
+    12: "",
+    1: "",
+    2: "",
+    3: "",
+    4: "",
+    5: "",
+}
+
+$("#currentDay").text(dayjs().format("MMM Do, YYYY"));
