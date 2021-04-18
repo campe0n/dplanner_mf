163b1f44c6f1ce99413c2f6c1b5b741272e65a93
Melvin Finn <melvinrf@Outlook.com>
2021-04-17 19:45:28 -0500

    with some help from connor I was able to write the proper syntax for targeting buttons and creating local storage elements. Now it's on to keeping values when the page is refresh

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,39 @@
+// time variables
 var currentDay = $("#currentDay");
-var container = $(".container");
 var curentHour = dayjs().hour();
 $('#currentDay').text(dayjs().format('MMMM Do, YYYY'));
-renderLastEvent();
-function saveEvent(event) {
+
+//Save
+$('button').on('click', function(event){
     event.preventDefault();
-    var schedule = {
-        nineam: $("#1").val(),
-        tenam: $("#2").val(),
-        elevenam: $("#3").val(),
-        twelveam: $("#4").val(),
-        onepm: $("#5").val(),
-        twopm: $("#6").val(),
-        threepm: $("#7").val(),
-        fourpm: $("#8").val(),
-        fivepm: $("#9").val(),
-    };
-    localStorage.setItem("event", JSON.stringify(schedule));
-    renderLastEvent();
-};
+    $('input[type="text"]').each(function(){
+        var id = $(this).attr('id');
+        var value = $(this).val();
+        localStorage.setItem(id, value);
+        })
+    })
 
 function renderLastEvent() {
-    var lastEvent = JSON.parse(localStorage.getItem("event"));
-    if (lastEvent !== null) {
-        $("#1").text(lastEvent.nineam);
-    }
-    console.log(lastEvent.nineam)
-}
+    var nineInput = localStorage.getItem("9");
+    var tenInput = localStorage.getItem("10");
+    var elevenInput = localStorage.getItem("11");
+    var twelveInput = localStorage.getItem("12");
+    var oneInput = localStorage.getItem("13");
+    var twoInput = localStorage.getItem("14");
+    var threeInput = localStorage.getItem("15");
+    var fourInput = localStorage.getItem("16");
+    var fiveInput = localStorage.getItem("17");
 
+    $("#9").text(nineInput);
+    $("#10").text(tenInput);
+    $("#11").text(elevenInput);
+    $("#12").text(twelveInput);
+    $("#1").text(oneInput);
+    $("#2").text(twoInput);
+    $("#3").text(threeInput);
+    $("#4").text(fourInput);
+    $("#5").text(fiveInput);
+
+}
+//call renderlastEvent (init)
+renderLastEvent();
